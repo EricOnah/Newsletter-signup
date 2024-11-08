a0043ae2bfb45309a752273c087d2e60b083090d
Eric Onah <ericdavid4u@gmail.com>
2024-11-08 17:46:12 +0100

    setup post route in app.js

--- app.js
+++ app.js
@@ -2,8 +2,8 @@ import express from "express";
 import bodyParser from "body-parser";
 import request from "request";
 import { fileURLToPath } from "url";
-import { dirname } from "path";
-import path from "path";
+import path, { dirname } from "path";
+// import path from "path";
 
 const app = express();
 const port = 3000;
@@ -11,10 +11,18 @@ const port = 3000;
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
-app.use(express.static(path.join(__dirname)));
+app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(express.static(path.join(__dirname)));
+app.use(express.static(__dirname));
 
 app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "/signup.html"));
+  //   res.sendFile(path.join(__dirname, "/signup.html"));
+  res.sendFile(__dirname + "/signup.html");
+});
+
+app.post("/", (req, res) => {
+  const { firstName, lastName, email } = req.body;
+  console.log(firstName, lastName, email);
 });
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
