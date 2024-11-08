3557c6149e71419f28974e43fc130bd1b62a2207
Eric Onah <ericdavid4u@gmail.com>
2024-11-08 17:04:44 +0100

    using path module to serve root directory as static to access both styles.css and image

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
 import express from "express";
 import bodyParser from "body-parser";
 import request from "request";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
 
 const app = express();
 const port = 3000;
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+app.use(express.static(path.join(__dirname)));
+
+app.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname, "/signup.html"));
+});
 
 app.listen(port, () => console.log(`Server running on port ${port}`));
